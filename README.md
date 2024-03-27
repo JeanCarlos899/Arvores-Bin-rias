1c5dea92f036e5de185a8234fdc31d873e60aaba
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2024-03-27 12:06:23 -0300

    Atualizações na implementação da árvore AVL

---
+++ README.md
@@ -0,0 +1,29 @@
+# Atividade sobre árvore AVL
+Nesta atividade foi proposta a implementação de uma árvore AVL para a disciplina de Estrutura de Dados II. A árvore AVL é uma árvore binária de busca balanceada, onde a diferença de altura entre as subárvores esquerda e direita de cada nó é no máximo 1. A implementação foi feita na linguagem de programação C.
+
+## Como compilar
+
+Para compilar o programa, basta executar o comando abaixo:
+
+```
+gcc -o main .\main.c .\AVL.c
+```
+
+## Como executar
+
+Para executar o programa, basta executar o comando abaixo:
+
+```
+.\main.exe
+```
+
+## Funcionalidades
+
+O programa possui as seguintes funcionalidades:
+
+1. Inicializar árvore AVL
+2. Inserir elemento
+3. Buscar elemento
+4. Conta o número de elementos da árvore
+5. imprime a árvore
+6. Remover elemento da árvore
--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 
 int main(int argc, char **argv)
 {
-    PONT r;
-    int v, b, n;
+    PONT raiz;
+    int valor, b, n;
     int continuar = 1;
 
     do
     {
         printf("\n\tMenu de Opcao\n\n");
         printf("1. Inicializa a arvore \n");
-        printf("2. Insere na arvore\n");
-        printf("3. Buscar um valor na arvore\n");
-        printf("4. Conta o numero de nos da arvore\n");
-        printf("5. Imprime a arvore\n");
-        printf("6. Remove um no da arvore\n");
+        printf("2. Insere na arvore AVL\n");
+        printf("3. Buscar um valor na arvore AVL\n");
+        printf("4. Conta o numero de nos da arvore AVL\n");
+        printf("5. Imprime a arvore AVL\n");
+        printf("6. Remove um no da arvore AVL\n");
         printf("0. Sair\n");
 
         scanf("%d", &continuar);
@@ -25,22 +25,22 @@ int main(int argc, char **argv)
         switch (continuar)
         {
         case 1:
-            r = inicializa();
+            raiz = inicializa();
             printf("Arvore criada com sucesso");
             break;
 
         case 2:
             printf("Digite um numero para inserir na arvore:");
-            scanf("%d", &v);
-            PONT no = criaNo(v);
-            r = insere(r, v);
+            scanf("%d", &valor);
+            PONT no = criaNo(valor);
+            raiz = insere(raiz, valor);
             printf("No inserido com sucesso");
             break;
 
         case 3:
             printf("Digite um valor para pesquisar na arvore");
             scanf("%d", &b);
-            PONT resultado = contem(b, r);
+            PONT resultado = contem(b, raiz);
             if (resultado == NULL)
                 printf("O valor %d nao foi localizado\n", b);
             else
@@ -48,23 +48,24 @@ int main(int argc, char **argv)
             break;
 
         case 4:
-            n = numeroNos(r);
+            n = numeroNos(raiz);
             printf("A arvore possui: %d nos\n", n);
             break;
 
         case 5:
-            exibirArvore(r);
+            exibirArvore(raiz);
             break;
 
         case 6:
             printf("Digite um valor para remover da arvore:");
-            scanf("%d", &v);
-            r = removeNo(r, v);
+            scanf("%d", &valor);
+            raiz = removeNo(raiz, valor);
             printf("Valor removido com sucesso");
             break;
 
         case 0:
             break;
+
         default:
             printf("Digite uma opcao valida\n");
         }
